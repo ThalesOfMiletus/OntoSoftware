19fa51d7126df22d864ee88dab5885a684222b11
Thales Martins <106634741+ThalesOfMiletus@users.noreply.github.com>
2023-05-17 17:31:48 -0300

    Update urls.py

--- ontosoftware/urls.py
+++ ontosoftware/urls.py
@@ -16,8 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from main.views import CadastrosViews
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('cadastros/', admin.site.urls),
+    path('cadastros/', CadastrosViews.as_view(), name='cadastros'),
 ]
